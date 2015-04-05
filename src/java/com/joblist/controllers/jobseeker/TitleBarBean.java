8ca9d106924b8da84324b139c939fbd7b1947818
esa <esa@nomail.fi>
2015-04-05 13:15:27 +0300

    ui tuunailut, turhat koodit pois.

--- src/java/com/joblist/controllers/jobseeker/TitleBarBean.java
+++ src/java/com/joblist/controllers/jobseeker/TitleBarBean.java
@@ -18,55 +18,21 @@ import javax.faces.context.FacesContext;
 @Named(value = "jobSeekerTitleBarBean")
 @SessionScoped
 public class TitleBarBean implements Serializable {
-    private boolean showUploadCV;
-    private boolean showBack;
 
     /**
      * Creates a new instance of TitleBarBean
      */
     public TitleBarBean() {
     }
-    
-    public void show(boolean showUploadCV) 
-    {
-        setShowUploadCV(showUploadCV);
-    }
-    
-    public void withBrowserHistoryBackShow(boolean showUploadCV)
-    {
-        setShowUploadCV(showUploadCV);
-        showBack = true;
-    }
 
     public String logout() {
         FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
         return "/jobseeker/login?faces-redirect=true";
-    }
-
-    /**
-     * @return the showUploadCV
-     */
-    public boolean isShowUploadCV() {
-        return showUploadCV;
-    }
-
-    /**
-     * @param showUploadCV the showUploadCV to set
-     */
-    public void setShowUploadCV(boolean showUploadCV) {
-        this.showUploadCV = showUploadCV;
     }
     
     public String getUsername() {
         Map<String, Object> sessionMap =
                     FacesContext.getCurrentInstance().getExternalContext().getSessionMap();
         return sessionMap.get("j_username").toString();
-    }
-    
-    /**
-     * @return the showBack
-     */
-    public boolean isShowBrowserHistoryBack() {
-        return showBack;
     }
 }
